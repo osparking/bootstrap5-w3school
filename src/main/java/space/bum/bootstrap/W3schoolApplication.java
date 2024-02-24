b2a51d9f13e2278ce52c8a523032575a4ad904e5
osparking <jbpark03@gmail.com>
2024-02-24 10:10:53 +0900

    bootstrap 5 학습용 프로젝트 초기

---
+++ src/main/java/space/bum/bootstrap/W3schoolApplication.java
@@ -0,0 +1,13 @@
+package space.bum.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3schoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3schoolApplication.class, args);
+	}
+
+}
